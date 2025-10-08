ce1d7296eb53dad1aa3c5bad8a83d934e0748ed3
Srusti-Priya55 <kspriya@cisco.com>
2025-10-08 11:33:53 +0530

    vpn updated

--- test/specs/vpn-connect.e2e.ts
+++ test/specs/vpn-connect.e2e.ts
@@ -341,7 +341,7 @@ const GROUP_TXT = process.env.CSC_GROUP || 'Split Inc (AAA)'
 const FLOW = process.env.CURRENT_FLOW || 'Adhoc';
 
 // We will move to the **7th** item to avoid selecting the one above your target
-const GROUP_IDX = Number(process.env.GROUP_INDEX || 7)
+const GROUP_IDX = Number(process.env.GROUP_INDEX || 8)
 // ------------------------------------------------
 
 describe(`${FLOW} - VPN CONNECTION`, () => {
